4eac0d4af4bda2aae4f716e25e398d84aa3d9cda
Christian Salas <chris@christiansalas.dev>
2022-12-26 01:27:36 -0800

    .

--- app.py
+++ app.py
@@ -1,1 +1,6 @@
 from models import User
+from flask import Flask, render_template, redirect, url_for
+
+User1 = User(user_name = "toby", email = "toby@aol.com", password = "ilovepablo")
+
+User1.save()
