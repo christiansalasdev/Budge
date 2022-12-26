a6d30ae8310ad99578f6a4705628876cd57105a1
Christian Salas <chris@christiansalas.dev>
2022-12-26 12:11:18 -0800

    Added several templates, plus login mechanism WIP

--- app.py
+++ app.py
@@ -1,6 +1,50 @@
-from models import User
-from flask import Flask, render_template, redirect, url_for
+from datetime import datetime
+from models import User, Bill, Paycheck
+from flask import Flask, render_template, redirect, url_for, request
+from flask_login import LoginManager, login_user
 
-User1 = User(user_name = "toby", email = "toby@aol.com", password = "ilovepablo")
+login_manager = LoginManager()
 
-User1.save()
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = "8lJFz9pnU6l0xXT4RqkQmB3XV_lmr1TwSY-WMFEsGec"
+
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.objects.get(id = user_id)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/app/create_account", methods=['GET', 'POST'])
+def create_account():
+    if request.method == 'GET':
+        return render_template("create_account.html")
+    if request.method == 'POST':
+        user_name = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+        user = User(user_name = user_name, email = email, password = password)
+        user.save()
+    return redirect(url_for("index"))
+
+@app.route("/app/login", methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template("login.html")
+    if request.method == 'POST':
+        user_name = request.form['username']
+        password = request.form['password']
+        user = User.objects.get(user_name = user_name)
+        verified = user.verify_user(password)
+        if verified:
+            login_user(user)
+        else:
+            return "A user with that username does not exist or the password is incorrect."
+    return redirect(url_for("index"))
+
--- models.py
+++ models.py
@@ -1,21 +1,62 @@
 from mongoengine import *
 import hashlib
 
+DEV_DB_URI = "mongodb://localhost:27017/budge"
+
+connect(host=DEV_DB_URI)
+
+
 class User(Document):
     id = SequenceField(primary_key=True)
     user_name = StringField()
     email = EmailField()
     password = StringField() 
+    is_active = BooleanField(default=True)
+
+    def hash_password(self, password):
+        hashed_password = hashlib.sha256(password.encode()).hexdigest()
+        return hashed_password
+
+    def verify_user(self, password):
+        hashed_password = hashlib.sha256(password.encode()).hexdigest()
+        if hashed_password == self.password:
+            return True
+        else:
+            return False
 
     def save(self, *args, **kwargs):
-        self.password = hashlib.sha256(self.password.encode()).hexdigest()
+        self.password = self.hash_password(self.password)
         return super(User, self).save(*args, **kwargs)
+
+    def is_authenticated(self):
+        return True
+    
+    def is_active(self):
+        return self.is_active
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return str(self.id)
 
 class Bill(Document):
     id = SequenceField(primary_key=True)
-    user_id = ReferenceField(User)
+    payer = ReferenceField(User)
     name = StringField()
-    due_date = DateField()
+    due_date = DateTimeField()
     amount = DecimalField()
+    recurring = BooleanField(default=True)
     paid = BooleanField(default=False)
+
+    def mark_as_paid(self):
+        self.paid = True
+        self.save()
+
+class Paycheck(Document):
+    id = SequenceField(primary_key=True)
+    payee = ReferenceField(User)
+    amount = DecimalField()
+    pay_date = DateTimeField()
+
 
