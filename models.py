a263664acee42f721a884ad3ae970cf74b53e2e9
Christian Salas <chris@christiansalas.dev>
2022-12-26 10:23:49 -0800

    Added Bill class

--- models.py
+++ models.py
@@ -1,7 +1,21 @@
 from mongoengine import *
+import hashlib
 
 class User(Document):
     id = SequenceField(primary_key=True)
     user_name = StringField()
     email = EmailField()
     password = StringField() 
+
+    def save(self, *args, **kwargs):
+        self.password = hashlib.sha256(self.password.encode()).hexdigest()
+        return super(User, self).save(*args, **kwargs)
+
+class Bill(Document):
+    id = SequenceField(primary_key=True)
+    user_id = ReferenceField(User)
+    name = StringField()
+    due_date = DateField()
+    amount = DecimalField()
+    paid = BooleanField(default=False)
+
